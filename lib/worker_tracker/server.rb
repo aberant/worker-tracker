0b8a59dd23d2d4026b8cbe4ba514ffd84c1d2b87
aberant <qzzzq1@gmail.com>
2010-10-03 13:14:58 -0500

    setting up the server index along with stylessheets and layout

--- lib/worker_tracker/server.rb
+++ lib/worker_tracker/server.rb
@@ -4,5 +4,31 @@ require 'erb'
 module WorkerTracker
   class Server < Sinatra::Base
     dir = File.dirname(File.expand_path(__FILE__))
+
+    set :views,  "#{dir}/server/views"
+    set :public, "#{dir}/server/public"
+    set :static, true
+
+    helpers do
+      include Rack::Utils
+
+      def path_prefix
+        request.env['SCRIPT_NAME']
+      end
+
+      def url(*path_parts)
+        [ path_prefix, path_parts ].join("/").squeeze('/')
+      end
+      alias_method :u, :url
+
+    end # helpers
+
+    def show(page, layout = true)
+      erb page.to_sym, {:layout => layout}
+    end
+
+    get "/" do
+      show :index
+    end
   end
 end
